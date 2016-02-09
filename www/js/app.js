c8da2b606058e7fcc7affdf6ff63943ed9d34c50
Vishnu Venkateswaran <vishnuv1228@gmail.com>
2016-02-09 00:46:54 -0800

    Figured out page navigation within goals, changed theme and completed task completion page

--- www/js/app.js
+++ www/js/app.js
@@ -5,37 +5,56 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-.config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('tabs', {
-        url: '/tab',
-        abstract: true,
-        templateUrl: 'templates/tabs.html'
-    })
-    .state('tabs.create_goal', {
-        url: '/create_goal',
-        views: {
-            'create_goal-tab' : {
-                templateUrl: 'templates/create_goal.html'
-            }
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
         }
     });
+})
+
+.controller('sampleController', function ($scope) {
+    $scope.numberSelection = 1500;
+
+    // To get value from range
+    //    $scope.showMeTheNumber = function(){
+    //    alert($scope.numberSelection); 
+})
+
+
+
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('tabs', {
+            url: '/tab',
+            abstract: true,
+            templateUrl: 'templates/tabs.html'
+        })
+        .state('tabs.create_task', {
+            url: '/create_task',
+            views: {
+                'create_goal-tab': {
+                    templateUrl: 'templates/create_task.html',
+                }
+            }
+        })
+        .state('tabs.create_goal', {
+            url: '/create_goal',
+            views: {
+                'create_goal-tab': {
+                    templateUrl: 'templates/create_goal.html'
+                }
+            }
+        });
     $urlRouterProvider.otherwise('/tab/create_goal');
 });
